42cf8cc9f645f178be9c3cfa65a1c281c17af6ab
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2019-12-30 14:23:09 +0700

    Update code

--- pythainlu/intent_classification/MultinomialNB.py
+++ pythainlu/intent_classification/MultinomialNB.py
@@ -5,8 +5,10 @@ from sklearn.pipeline import Pipeline
 from pythainlp.ulmfit import process_thai
 from sklearn.metrics import accuracy_score
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer, TfidfTransformer
+from sklearn.feature_selection import SelectKBest, chi2
 nb = Pipeline([('vect', CountVectorizer(tokenizer=process_thai, ngram_range=(1,2))),
                ('tfidf', TfidfTransformer()),
+               ('chi2', SelectKBest(chi2, k='all')),
                ('clf', MultinomialNB()),
               ])
 
@@ -23,15 +25,15 @@ def train(name,train_data:List[tuple],test_data=None)->tuple:
     X_data=[]
     y_data=[]
     for sent in train_data:
-        X_data.append([line[0] for line in sent])
-        y_data.append([line[-1] for line in sent])
+        X_data.append(sent[0])
+        y_data.append(sent[1])
     nb.fit(X_data, y_data)
     if test_data!=None:
         X_test=[]
         y_test=[]
         for sent in test_data:
-            X_test.append([line[0] for line in sent])
-            y_test.append([line[-1] for line in sent])
+            X_test.append(sent[0])
+            y_test.append(sent[1])
         y_pred = nb.predict(X_test)
         return (nb, accuracy_score(y_pred, y_test))
     return (nb,)
--- pythainlu/intent_classification/__init__.py
+++ pythainlu/intent_classification/__init__.py
@@ -1,5 +1,17 @@
 # -*- coding: utf-8 -*-
-from .naive_bayes import train,predict
-class model(object):
-    def __init__(self,name,path,type):
-        pass
+import dill
+
+class model():
+    def __init__(self,name,path=None,model_name=""):
+        self.name = name
+        self.path = path
+        self.model_name = model_name
+        self.model_data = None
+        self.load_model()
+    def load_model(self):
+        if self.path is not None:
+            with open(self.path ,'rb') as f:
+                self.model_data = dill.load(f)
+    def train(self):
+        if self.name == "naive_bayes":
+            pass
